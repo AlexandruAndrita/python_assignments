46300d9bd7eb35094e086027abfa5d1d065eab56
Alexandru Andrita <andu.andritza@gmail.com>
2022-11-11 01:16:55 +0200

    kw added

--- programming_in_python/homework IV/submission 4/ex4.py
+++ programming_in_python/homework IV/submission 4/ex4.py
@@ -1,3 +1,6 @@
+import math
+
+
 def number_digits(value):
     count=0
     while value:
@@ -7,21 +10,28 @@ def number_digits(value):
 
 
 def round_(number, ndigits: int = None):
-    fractional_part=number%1
+    fractional_part = number % 1
+
     if ndigits==None:
         if fractional_part>=0.5:
             return int(number+1.0)
         elif fractional_part<0.5:
-            return int(number-1.0)
-    elif ndigits>=0:
-        pass
-    elif ndigits<0:
-        number=int(number)
-        digits=number_digits(number) #number of digits of the number
-        ndigits*=(-1)
-        number=str(number)
-        pass
+            return int(number)
 
+    elif ndigits>=0:
+        number=math.ceil(number * math.pow(10, ndigits))
+        number/=math.pow(10, ndigits)
+        return number
+
+    elif ndigits<0:
+        tmp=int(number)
+        number_copy=str(tmp)
+        if ndigits*(-1)>len(number_copy):
+            return 0.0
+        else:
+            number=math.ceil(number * math.pow(10, ndigits))
+            number/=math.pow(10, ndigits)
+            return number
 
 
 print("round_(777.777) = {}".format(round_(777.777)))
@@ -33,13 +43,4 @@ print("round_(777.777, 4) = {}".format(round_(777.777, 4)))
 print("round_(777.777, -1) = {}".format(round_(777.777, -1)))
 print("round_(777.777, -2) = {}".format(round_(777.777, -2)))
 print("round_(777.777, -3) = {}".format(round_(777.777, -3)))
-
-print(777.786%1)
-print(777.786%0.1)
-print(777.786%0.2)
-print(777.786%0.3)
-print(777.786%3)
-
-import math
-
-print(math.floor(23.34))
+print("round_(777.777, -4) = {}".format(round_(777.777, -4)))
